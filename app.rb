0f7bc20fca58580c6826e635bf1be3f256b2dc19
RyanSnodgrass <res0428@yahoo.com>
2014-04-15 19:01:16 -0400

    added addition page

--- app.rb
+++ app.rb
@@ -4,24 +4,35 @@ require "sinatra/activerecord"
 #RACK_ENV=production rackup config.ru
 set :database, "sqlite3:///to_do_class_app.db"
 
+get "/favorite_movies" do
+	@favs = FavoriteMovie.all
+	erb :"fav_mov/index"
+end
+
 get "/error" do
 	"whoopsies try again"
 end
 
 get "/addition" do
+
 	erb :"fav_mov/addition_index"
 end
 
-get "/fav_mov_app" do	
+
+get "/remove" do
 	@favs = FavoriteMovie.find(params[:id])
-	erb :"fav_mov/index"
+	erb :"fav_mov/remove_index"
 end
 
-# get "/remove" do
-# 	@favs = FavoriteMovie.find(params[:id])
-# 	erb :"fav_mov/remove_index"
-# end
- 
+post "/add_post" do
+	# name = params[:mov_name]
+	# description = params[:mov_desc]
+	movie = FavoriteMovie.new(params[:id])
+	movie.title = params[:mov_name]
+	movie.description = params[:mov_desc]
+	movie.save
+	redirect "./favorite_movies"
+end
 # post "/favs" do
 # 	name = params[:mov_name]
 # 	description = params[:mov_desc]
@@ -31,7 +42,8 @@ end
 # end
 
 class FavoriteMovie < ActiveRecord::Base
-	def meet_criteria?
+	def add_movie
+
 		## validations
 	end
 end
